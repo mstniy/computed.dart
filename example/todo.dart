68f53acc3693112bf5910ba61f799c7bb949a27e
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2023-12-02 08:31:48 +0100

    Todo example using withPrev and functional react

---
+++ example/todo.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:built_collection/built_collection.dart';
+import 'package:computed/computed.dart';
+import 'package:tuple/tuple.dart';
+
+typedef TodoItem = String;
+
+typedef AppState = BuiltMap<int, TodoItem>;
+
+class FictionaryDatabase {
+  Future<AppState?> loadState() {
+    // 1 second delay for dramatic effect
+    print('Reading DB...');
+    return Future.delayed(
+        const Duration(seconds: 1), () => AppState({0: "todo1"}));
+  }
+
+  Future<void> saveState(AppState state) {
+    print('DB saving state: $state');
+    return Future.value();
+  }
+}
+
+final db = FictionaryDatabase();
+
+void main() async {
+  final uiDeleteController = StreamController<int>(
+      sync: true); // Use a sync controller to make debugging easier
+  final uiDelete = uiDeleteController.stream;
+  final uiUpdateController = StreamController<Tuple2<int, TodoItem>>(
+      sync: true); // Use a sync controller to make debugging easier
+  final uiUpdate = uiUpdateController.stream;
+
+  final uiCreateController = StreamController<TodoItem>(
+      sync: true); // Use a sync controller to make debugging easier
+  final uiCreate = uiCreateController.stream;
+
+  final dbFuture = db.loadState();
+  final largestId = Computed<int>.withPrev((prev) {
+    var id = prev;
+    uiCreate.react((p0) => id++, null);
+
+    return id;
+  },
+      initialPrev:
+          ((await dbFuture) ?? BuiltMap<int, String>()).keys.reduce(math.max));
+
+  final appState = Computed<Tuple2<bool, AppState>>.withPrev((prev) {
+    var newState = prev;
+    // Apply UI changes, if there are any
+    uiDelete.react(
+        (delId) => newState =
+            Tuple2(false, newState.item2.rebuild((p0) => p0.remove(delId))),
+        null);
+    uiUpdate.react(
+        (update) => newState = Tuple2(false,
+            newState.item2.rebuild((p0) => p0[update.item1] = update.item2)),
+        null);
+    uiCreate.react(
+        (todo) => newState = Tuple2(
+            false, newState.item2.rebuild((p0) => p0[largestId.use] = todo)),
+        null);
+    return newState;
+  }, initialPrev: Tuple2(true, (await dbFuture) ?? AppState()));
+
+  appState.listen((state) {
+    if (!state.item1) db.saveState(state.item2);
+  }, (e) => print('Exception: ${e.toString()}'));
+
+  await Future.delayed(const Duration(seconds: 2));
+
+  await Future.value();
+  uiCreateController.add('new todo');
+  await Future.value();
+  uiDeleteController.add(0); // this marks largestId as dirty. why?
+  await Future.value();
+  uiUpdateController.add(Tuple2(1, 'new todo edited'));
+  await Future.value();
+  uiDeleteController.add(1);
+  await Future.value();
+}
