9a95f4c8349948541cdcf9578e416174ace6188b
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2024-03-11 21:30:14 +0100

    Fix test

--- packages/computed/test/delayed_reeval_test.dart
+++ packages/computed/test/delayed_reeval_test.dart
@@ -31,7 +31,7 @@ void main() {
     final sub = c.listen((event) {
       lCnt++;
       lastEvent = event;
-    }, (e) => fail(e.toString()));
+    }, (e) => fail(e.toString()), false);
 
     s.add(0);
     expect(odcCnt, 1);
