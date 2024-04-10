bc7c75b00e6ed3c3fd08faaa4495ad466b71bc3d
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2024-04-10 02:34:11 +0200

    SnapshotChangeStreamComputedMap: Optimize

--- packages/computed_collections/lib/src/scs_computedmap.dart
+++ packages/computed_collections/lib/src/scs_computedmap.dart
@@ -1,5 +1,4 @@
 import 'package:computed/computed.dart';
-import 'package:computed/utils/computation_cache.dart';
 import 'package:computed_collections/change_event.dart';
 import 'package:computed_collections/icomputedmap.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
@@ -8,17 +7,13 @@ import 'computedmap_mixins.dart';
 
 /// An [IComputedMap] represented by a change and a snapshot stream.
 class SnapshotChangeStreamComputedMap<K, V>
-    with OperatorsMixin<K, V>, MockMixin<K, V>
+    with OperatorsMixin<K, V>
     implements IComputedMap<K, V> {
   final Computed<bool> isEmpty;
   final Computed<bool> isNotEmpty;
   final Computed<int> length;
   final Computed<ChangeEvent<K, V>> changes;
   final Computed<IMap<K, V>> snapshot;
-
-  final keyComputations = ComputationCache<K, V?>();
-  final containsKeyComputations = ComputationCache<K, bool>();
-  final containsValueComputations = ComputationCache<V, bool>();
 
   SnapshotChangeStreamComputedMap(
       Stream<ChangeEvent<K, V>> _changes, Stream<IMap<K, V>> _snapshot)
@@ -36,14 +31,31 @@ class SnapshotChangeStreamComputedMap<K, V>
         snapshot = $(() => _snapshot.use);
 
   @override
-  Computed<bool> containsKey(K key) =>
-      containsKeyComputations.wrap(key, () => snapshot.use.containsKey(key));
+  // TODO: ChangeStreamComputedMap does it better.
+  //  This approach re-computed the existance of all keys any time the snapshot changes,
+  //  which is inefficient.
+  Computed<bool> containsKey(K key) => $(() => snapshot.use.containsKey(key));
 
   @override
-  Computed<V?> operator [](K key) =>
-      keyComputations.wrap(key, () => snapshot.use[key]);
+  Computed<V?> operator [](K key) => $(() => snapshot.use[key]);
 
   @override
-  Computed<bool> containsValue(V value) => containsValueComputations.wrap(
-      value, () => snapshot.use.containsValue(value));
+  Computed<bool> containsValue(V value) =>
+      $(() => snapshot.use.containsValue(value));
+
+  static const _mockErrorMessage =
+      "Please mock the result of `.groupBy` instead.";
+
+  @override
+  void fix(IMap<K, V> value) => throw UnsupportedError(_mockErrorMessage);
+
+  @override
+  void fixThrow(Object e) => throw UnsupportedError(_mockErrorMessage);
+
+  @override
+  void mock(IComputedMap<K, V> mock) =>
+      throw UnsupportedError(_mockErrorMessage);
+
+  @override
+  void unmock() => throw UnsupportedError(_mockErrorMessage);
 }
