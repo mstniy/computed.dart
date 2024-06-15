1fa54f7cb4ef15458fb9314ccbb40a3dae186ed5
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2024-06-15 19:55:39 +0300

    groupBy tests: Also directly test the groups' change streams   Now they are independent of the snapshot streams, so they deserve to be tested separately

--- packages/computed_collections/test/groupby_test.dart
+++ packages/computed_collections/test/groupby_test.dart
@@ -29,6 +29,10 @@ void main() {
     final sub4 = $(() => m2.snapshot.use[2]?.snapshot.use).listen((event) {
       lastRes4 = event;
     }, null);
+    ChangeEvent<int, int>? lastRes5;
+    final sub5 = $(() => m2.snapshot.use[0]?.changes.use).listen((event) {
+      lastRes5 = event;
+    }, null);
     await Future.value();
     expect(lastRes1, {}.lock);
     for (var i = 0; i < 5; i++) {
@@ -37,89 +41,113 @@ void main() {
     expect(lastRes2, null);
     expect(lastRes3, null);
     expect(lastRes4, null);
+    expect(lastRes5, null);
     s.add(KeyChanges({0: ChangeRecordValue(1)}.lock)); // Add a new group
     await Future.value();
     expect(lastRes1!.keys, [1]);
     expect(lastRes2, null);
     expect(lastRes3, {0: 1}.lock);
     expect(lastRes4, null);
+    expect(lastRes5, null);
     // Change the value of an existing item, removing a group
     // + add a new group
     s.add(KeyChanges({0: ChangeRecordValue(2), 1: ChangeRecordValue(0)}.lock));
     await Future.value();
-    expect(lastRes1!.keys, containsAll([0, 2]));
+    expect(lastRes1!.keys, unorderedEquals([0, 2]));
     expect(lastRes2, {1: 0}.lock);
     expect(lastRes3, null);
     expect(lastRes4, {0: 2}.lock);
+    expect(lastRes5, null); // No changes, as the group just got created
     // Change the value of an existing item, while preserving its group
     // + Remove a group by removing an item
     s.add(KeyChanges(
         {0: ChangeRecordDelete<int>(), 1: ChangeRecordValue(3)}.lock));
-    expect(lastRes1!.keys, containsAll([0]));
+    expect(lastRes1!.keys, [0]);
     await Future
         .value(); // Wait for the microtask delay of the internal StreamController
     expect(lastRes2, {1: 3}.lock);
     expect(lastRes3, null);
     expect(lastRes4, null);
+    expect(lastRes5, KeyChanges({1: ChangeRecordValue(3)}.lock));
     // Add a value to an existing group
     s.add(KeyChanges({0: ChangeRecordValue(0)}.lock));
-    expect(lastRes1!.keys, containsAll([0]));
+    expect(lastRes1!.keys, [0]);
     await Future.value();
     expect(lastRes2, {0: 0, 1: 3}.lock);
     expect(lastRes3, null);
     expect(lastRes4, null);
+    expect(lastRes5, KeyChanges({0: ChangeRecordValue(0)}.lock));
     // Remove a value from an existing group, which has other elements
     s.add(KeyChanges({0: ChangeRecordDelete<int>()}.lock));
-    expect(lastRes1!.keys, containsAll([0]));
+    expect(lastRes1!.keys, [0]);
     await Future.value();
     expect(lastRes2, {1: 3}.lock);
     expect(lastRes3, null);
     expect(lastRes4, null);
+    expect(lastRes5, KeyChanges({0: ChangeRecordDelete<int>()}.lock));
     // Upstream replacement
     s.add(ChangeEventReplace({0: 0, 1: 1, 2: 3}.lock));
     await Future.value();
-    expect(lastRes1!.keys, containsAll([0, 1]));
+    expect(lastRes1!.keys, unorderedEquals([0, 1]));
     expect(lastRes2, {0: 0, 2: 3}.lock);
     expect(lastRes3, {1: 1}.lock);
     expect(lastRes4, null);
+    expect(
+        lastRes5,
+        KeyChanges({
+          0: ChangeRecordDelete<int>()
+        }.lock)); // No new change event, as the group got replaced with a new one
     // Change the group of an item, changing its group, but keeping its former group populated
     s.add(KeyChanges({0: ChangeRecordValue(1)}.lock));
-    expect(lastRes1!.keys, containsAll([0, 1]));
+    expect(lastRes1!.keys, unorderedEquals([0, 1]));
     await Future.value();
     expect(lastRes2, {2: 3}.lock);
     expect(lastRes3, {0: 1, 1: 1}.lock);
     expect(lastRes4, null);
+    expect(
+        lastRes5,
+        KeyChanges({0: ChangeRecordDelete<int>()}
+            .lock)); // Happens to be the same as the last event
     // Delete a key, removing a group, but a new key immediately re-creates it
     s.add(KeyChanges(
         {2: ChangeRecordDelete<int>(), 0: ChangeRecordValue(3)}.lock));
-    expect(lastRes1!.keys, containsAll([0, 1]));
+    expect(lastRes1!.keys, unorderedEquals([0, 1]));
     await Future.value();
     await Future.value();
     expect(lastRes2, {0: 3}.lock);
     expect(lastRes3, {1: 1}.lock);
     expect(lastRes4, null);
+    expect(
+        lastRes5,
+        KeyChanges(
+            {0: ChangeRecordValue(3), 2: ChangeRecordDelete<int>()}.lock));
     // Make multiple changes to groups in one upstream change
     s.add(KeyChanges({
       2: ChangeRecordValue(1),
       1: ChangeRecordValue(3),
       0: ChangeRecordValue(1),
     }.lock));
-    expect(lastRes1!.keys, containsAll([0, 1]));
+    expect(lastRes1!.keys, unorderedEquals([0, 1]));
     await Future.value();
     expect(lastRes2, {1: 3}.lock);
     expect(lastRes3, {0: 1, 2: 1}.lock);
     expect(lastRes4, null);
+    expect(
+        lastRes5,
+        KeyChanges(
+            {0: ChangeRecordDelete<int>(), 1: ChangeRecordValue(3)}.lock));
 
     sub1.cancel();
     sub2.cancel();
     sub3.cancel();
     sub4.cancel();
+    sub5.cancel();
   });
 
   test('initial computation works', () async {
     final m1 = ConstComputedMap({0: 1, 1: 2, 2: 3, 3: 4}.lock);
     final m2 = m1.groupBy((key, value) => key % 3);
-    expect((await getValue(m2.snapshot)).keys, [0, 1, 2]);
+    expect((await getValue(m2.snapshot)).keys, unorderedEquals([0, 1, 2]));
   });
   test('operator[] works', () async {
     final m1 = ConstComputedMap({0: 1, 1: 2, 2: 3, 3: 4}.lock);
